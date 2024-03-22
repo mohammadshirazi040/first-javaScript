8e4b4263b5c91f67355126139eb572935c1e7ef0
Mohammad Shirazi <m.shirazi040@gmail.com>
2024-03-22 10:21:17 +0100

    add arrays

---
+++ arrays/script.js
@@ -0,0 +1,23 @@
+// TODO: Implement the removeItem function
+
+function removeItem(arr, num) {
+  const newar = arr.slice();
+  newar.splice(num - 1, 1);
+
+  return newar;
+}
+
+const ainmals = ["Dog", "Cat", "Lion"];
+
+console.log(removeItem(ainmals, 1));
+// result should be: ["Cat", "Lion"]
+
+console.log(ainmals);
+// result should be still: ["Dog", "Cat", "Lion"]
+
+const fruits = ["Watermelon", "Banana", "Cherry", "Kiwi", "Pineapple", "Apple"];
+console.log(removeItem(fruits, 3));
+// result should be: ["Watermelon", "Banana", "Kiwi", "Pineapple", "Apple"]
+
+console.log(fruits);
+// result should be still: ["Watermelon", "Banana", "Cherry", "Kiwi", "Pineapple", "Apple"]
